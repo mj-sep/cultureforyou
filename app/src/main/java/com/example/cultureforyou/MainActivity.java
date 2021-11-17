24669dc83de9d9d023659939bb93c6ec2e97b202
minjoo <kayla.sep9@gmail.com>
2021-11-18 02:39:55 +0900

    firebase push test

--- app/src/main/java/com/example/cultureforyou/MainActivity.java
+++ app/src/main/java/com/example/cultureforyou/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.cultureforyou;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity<val> extends AppCompatActivity {
+
+    private Button sendbt;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference("Art");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                // 버튼 누르면 수행 할 명령
+                databaseReference.child("message").push().setValue("2");
+            }
+        });
     }
+
+
 }
