d2c0f4ffb5d414d8da89b4514de058163a26c81e
minjoo <kayla.sep9@gmail.com>
2021-12-13 01:16:25 +0900

    메인 -> 플레이리스트 기능 연결

--- app/src/main/java/com/example/cultureforyou/PopupActivity.java
+++ app/src/main/java/com/example/cultureforyou/PopupActivity.java
@@ -1,15 +1,35 @@
 package com.example.cultureforyou;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.stream.Stream;
+
 public class PopupActivity extends AppCompatActivity {
+    private ImageButton feeling_intense_btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feeling_list_activity);
+
+        // 버튼 및 뷰 정의
+        feeling_intense_btn = findViewById(R.id.feeling_intense_btn);
+
+        // 강렬함 버튼만 활성화된 상태 (12/12)
+        feeling_intense_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StreamingActivity.class);
+                intent.putExtra("selectmood", "a1");
+                startActivity(intent);
+            }
+        });
     }
 }
