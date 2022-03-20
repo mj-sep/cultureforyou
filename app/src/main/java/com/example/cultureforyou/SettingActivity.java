a744fb28f6ac619378d23fc2e4b33f9a089c00ba
hyunj <hyunjung9934@naver.com>
2022-03-20 14:18:16 +0900

    설정 페이지

--- app/src/main/java/com/example/cultureforyou/SettingActivity.java
+++ app/src/main/java/com/example/cultureforyou/SettingActivity.java
@@ -1,34 +1,20 @@
 package com.example.cultureforyou;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfileActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
-    ImageButton btn_profile_edit;
     ImageButton btn_backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
-
-        btn_profile_edit = findViewById(R.id.profile_img_edit_button);
-
-        btn_profile_edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),ProfileEditActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.setting_page);
 
         btn_backward = findViewById(R.id.backward_button);
 
@@ -40,4 +26,5 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
+
 }
